d1146d672d1872e6cdc6349360b97b7cfb00511b
sagit <minhquoc1423@gmail.com>
2022-08-24 11:58:18 +0700

    Change code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,60 +1,66 @@
 import implementations.*;
 import org.apache.commons.lang.NullArgumentException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 
 public class Main {
     static Stack<String> stack = new Stack<>();
     static Queue<String> queue = new Queue<>();
-    static String[] strings;
-    public static void main(String[] args) throws IOException{
-        enterMessage();
+    static String messagesFile = "./src/messages.txt";
+
+    public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
         transferMessages();
         processMessages();
         printMessages();
-        long stopTime = System.currentTimeMillis();
-        long elaspedTime = stopTime - startTime;
+        long endTime = System.currentTimeMillis();
+        long elaspedTime = endTime - startTime;
         System.out.println("The system took " + elaspedTime + "ms to run...");
     }
 
     public static void enterMessage() throws IOException {
-        System.out.println("Enter your message");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        strings = br.readLine().split("\\.");
+//        System.out.println("Enter your message");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        strings = br.readLine().split("\\.");
+
     }
     public static void transferMessages(){
         try{
-            for (int i = 0; i < strings.length; i++) {
-                if (strings[i].length() > 250){
-                    throw new IndexOutOfBoundsException("The message characters must be less than 250 character!!! Please retype");
-                }else if(strings[i].equals("")){
-                    throw new NullArgumentException("You haven't typed anything yet!!! Please retype");
-                }else {
-                    queue.offer(strings[i]);
-                    System.out.println("Message has been sent successfully!!!");
+            FileReader fr = new FileReader(messagesFile);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine())!=null){
+                try {
+                    if (line.length() > 250) {
+                        throw new IndexOutOfBoundsException("The message characters must be less than 250 character!!! Please retype");
+                    }else if(line.equals("")){
+                        throw new NullArgumentException("You haven't typed anything yet!!! Please retype");
+                    }else {
+                        long startTime = System.currentTimeMillis();
+                        if (!line.equals("finish")){
+                            queue.offer(line);
+                            System.out.println("Message has been sent successfully!!!");
+                        }
+                        long endTime1 = System.currentTimeMillis();
+                    }
+                }catch (IndexOutOfBoundsException iae) {
+                    System.out.println(iae.getMessage());
+                }catch (NullArgumentException nae){
+                    System.out.println(nae.getMessage());
                 }
             }
-            while (!queue.isEmpty()){
-                String message = queue.poll();
-                stack.push(message);
-            }
-        }catch (IndexOutOfBoundsException ioobe){
-            System.out.println(ioobe.getMessage());
-        }catch (NullArgumentException nae){
-            System.out.println(nae.getMessage());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     public static void processMessages(){
-        while (!stack.isEmpty()){
-            queue.offer(stack.pop() + ".");
-        }
         while (!queue.isEmpty()){
-            String message = queue.poll();
-            stack.push(message);
+            stack.push(queue.poll());
         }
     }
     public static void printMessages(){
